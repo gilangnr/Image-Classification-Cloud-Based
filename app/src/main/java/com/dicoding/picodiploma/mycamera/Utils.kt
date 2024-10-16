7b3f88a345c2374568a7beeda1d13e6a56d76fff
Gilang <gilangnurrizki173@gmail.com>
2024-10-16 11:31:10 +0700

    fix bug on rotate

--- app/src/main/java/com/dicoding/picodiploma/mycamera/Utils.kt
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/Utils.kt
@@ -4,10 +4,13 @@ import android.content.ContentValues
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
 import android.net.Uri
 import android.os.Build
 import android.os.Environment
 import android.provider.MediaStore
+import androidx.annotation.RequiresApi
 import androidx.core.content.FileProvider
 import java.io.ByteArrayOutputStream
 import java.io.File
@@ -70,7 +73,7 @@ fun uriToFile(imageUri: Uri, context: Context): File {
 
 fun File.reduceFileImage(): File {
     val file = this
-    val bitmap = BitmapFactory.decodeFile(file.path)
+    val bitmap = BitmapFactory.decodeFile(file.path).getRotatedBitmap(file)
     var compressQuality = 100
     var streamLength: Int
     do {
@@ -83,4 +86,27 @@ fun File.reduceFileImage(): File {
     bitmap?.compress(Bitmap.CompressFormat.JPEG, compressQuality, FileOutputStream(file))
 
     return file
+}
+
+@RequiresApi(Build.VERSION_CODES.Q)
+fun Bitmap.getRotatedBitmap(file: File): Bitmap? {
+    val orientation = ExifInterface(file).getAttributeInt(
+        ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED
+    )
+
+    return when (orientation) {
+        ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(this, 90F)
+        ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(this, 180F)
+        ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(this, 270F)
+        ExifInterface.ORIENTATION_NORMAL -> this
+        else -> this
+    }
+}
+
+fun rotateImage(source: Bitmap, angle: Float): Bitmap? {
+    val matrix = Matrix()
+    matrix.postRotate(angle)
+    return Bitmap.createBitmap(
+        source, 0, 0, source.width, source.width, matrix, true
+    )
 }
