f79afbfe2336aa332dda5c64fdd9b85e423055a6
Gilang <gilangnurrizki173@gmail.com>
2024-10-16 11:10:58 +0700

    fix bug ukuran file

--- app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
@@ -13,8 +13,13 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
+import androidx.lifecycle.lifecycleScope
 import com.dicoding.picodiploma.mycamera.CameraActivity.Companion.CAMERAX_RESULT
+import com.dicoding.picodiploma.mycamera.data.api.ApiConfig
+import com.dicoding.picodiploma.mycamera.data.api.FileUploadResponse
 import com.dicoding.picodiploma.mycamera.databinding.ActivityMainBinding
+import com.google.gson.Gson
+import kotlinx.coroutines.launch
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.asRequestBody
@@ -108,7 +113,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun uploadImage() {
         currentImageUri?.let { uri ->
-            val imageFile = uriToFile(uri, this)
+            val imageFile = uriToFile(uri, this).reduceFileImage()
             Log.d("Image Classification File", "showImage: ${imageFile.path}")
             showLoading(true)
 
@@ -118,6 +123,27 @@ class MainActivity : AppCompatActivity() {
                 imageFile.name,
                 requestImageFile
             )
+            lifecycleScope.launch {
+                try {
+                    val apiService = ApiConfig.getApiService()
+                    val successResponse = apiService.uploadImage(multipartBody)
+                    with(successResponse.data) {
+                        binding.resultTextView.text = if (isAboveThreshold == true) {
+                            showToast(successResponse.message.toString())
+                            String.format("%s with %.2f%%", result, confidenceScore)
+                        } else {
+                            showToast("Model is predicted successfully but under threshold.")
+                            String.format("Please use the correct picture because the confidience score is %.2f%%", confidenceScore)
+                        }
+                    }
+                    showLoading(false)
+                } catch (e: retrofit2.HttpException) {
+                    val erroBody = e.response()?.errorBody()?.string()
+                    val errorResponse = Gson().fromJson(erroBody, FileUploadResponse::class.java)
+                    showToast(errorResponse.message.toString())
+                    showLoading(false)
+                }
+            }
         } ?: showToast(getString(R.string.empty_image_warning))
     }
 
